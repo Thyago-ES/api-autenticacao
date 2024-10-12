38b4bf31c4649e61707f18665a3e3a768c5d8180
Thyago-ES <thyagorafael57@gmail.com>
2024-10-11 21:56:43 -0300

    feat: create sequelize config and insert database credentials

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize({
+	dialect: "postgres",
+	host: process.env.DB_HOST,
+	username: process.env.DB_USER,
+	password: process.env.DB_PASS,
+	database: process.env.DB_NAME,
+});
+
+module.exports = sequelize;
