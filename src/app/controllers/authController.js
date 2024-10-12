308e493e5aa16948bb4eeadb1520f8800a8ec6e2
Thyago-ES <thyagorafael57@gmail.com>
2024-10-11 21:48:38 -0300

    feat: create autentication controller

---
+++ src/app/controllers/authController.js
@@ -0,0 +1,44 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+exports.register = async (req, res) => {
+	try {
+		let user = await User.findOne({ where: { email: req.body.email } });
+		if (user) {
+			return res.status(400).json({ error: "Email já registrado" });
+		}
+
+		user = await User.create(req.body);
+		res.status(201).json({ message: "Usuário criado com sucesso" });
+	} catch (err) {
+		return res.status(500).json({ error: "Erro ao registrar usuário" });
+	}
+};
+
+exports.login = async (req, res) => {
+	try {
+		const { email, password } = req.body;
+
+		let user = await User.findOne({ where: { email } });
+		if (!user) {
+			return res.status(404).json({ error: "Usuário não encontrado" });
+		}
+
+		if (!(await bcrypt.compare(password, user.password))) {
+			return res.status(400).json({ error: "A senha está incorreta" });
+		}
+
+		const token = jwt.sign(
+			{ id: user.id, role: user.role },
+			process.env.JWT_SECRET,
+			{
+				expiresIn: "1h",
+			}
+		);
+
+		return res.status(200).json({ token });
+	} catch (err) {
+		return res.status(500).json({ error: "Erro ao fazer login" });
+	}
+};
