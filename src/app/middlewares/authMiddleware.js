115b2821481ac4b0ca597b93da78693844364a7e
Thyago-ES <thyagorafael57@gmail.com>
2024-10-11 21:54:10 -0300

    feat: create authentication miiddleware

---
+++ src/app/middlewares/authMiddleware.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+
+const authMiddleware = async (req, res, next) => {
+	try {
+		const token = req.header("Authorization").split(" ")[1];
+		if (!token) {
+			return res.status(401).json({ error: "Token não fornecido" });
+		}
+
+		const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+		req.userId = decoded.id;
+		req.userRole = decoded.role;
+
+		next();
+	} catch (err) {
+		return res.status(500).json({ error: "Token inválido" });
+	}
+};
+
+module.exports = authMiddleware;
