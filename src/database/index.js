41ff8da46e7a3f6c936e327541f36ee105ad4bbd
Thyago-ES <thyagorafael57@gmail.com>
2024-10-14 22:52:05 -0300

    feat: add class Database to clear the connection and initialize models

---
+++ src/database/index.js
@@ -0,0 +1,19 @@
+const { Sequelize } = require("sequelize");
+const config = require("../config/database");
+
+const User = require("../app/models/User");
+
+const models = [User];
+
+class Database {
+	constructor() {
+		this.connection = new Sequelize(config);
+		this.init();
+	}
+
+	init() {
+		models.forEach((model) => model.init(this.connection));
+	}
+}
+
+module.exports = new Database();
