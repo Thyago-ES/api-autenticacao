d6898979d162806b93ac9f78c6062fedd89cd858
Thyago-ES <thyagorafael57@gmail.com>
2024-10-14 22:50:32 -0300

    feat: change to only configs for database connection

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,9 @@
-const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
-const sequelize = new Sequelize({
+module.exports = {
 	dialect: "postgres",
 	host: process.env.DB_HOST,
 	username: process.env.DB_USER,
 	password: process.env.DB_PASS,
 	database: process.env.DB_NAME,
-});
-
-module.exports = sequelize;
+};
