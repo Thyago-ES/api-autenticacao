48dca034c38c4c9e79be218964afa2002065cf9f
Thyago-ES <thyagorafael57@gmail.com>
2024-10-14 22:49:28 -0300

    feat: change to class User for clean code

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,40 +1,43 @@
-const { DataTypes } = require("sequelize");
+const { DataTypes, Model } = require("sequelize");
 const bcrypt = require("bcryptjs");
-const sequelize = require("../../config/database");
 
-const User = sequelize.define(
-	"User",
-	{
-		name: {
-			type: DataTypes.STRING,
-			allowNull: false,
-			field: "nome",
-		},
-		email: {
-			type: DataTypes.STRING,
-			allowNull: false,
-			unique: true,
-		},
-		password: {
-			type: DataTypes.STRING,
-			allowNull: false,
-			field: "senha",
-		},
-		role: {
-			type: DataTypes.ENUM("admin", "user"),
-			allowNull: false,
-			defaultValue: "user",
-		},
-	},
-	{
-		tableName: "usuarios",
-		hooks: {
-			beforeCreate: async (user) => {
-				user.password = await bcrypt.hash(user.password, 8);
+class User extends Model {
+	static init(sequelize) {
+		super.init(
+			{
+				name: {
+					type: DataTypes.STRING,
+					allowNull: false,
+					field: "nome",
+				},
+				email: {
+					type: DataTypes.STRING,
+					allowNull: false,
+					unique: true,
+				},
+				password: {
+					type: DataTypes.STRING,
+					allowNull: false,
+					field: "senha",
+				},
+				role: {
+					type: DataTypes.ENUM("admin", "user"),
+					allowNull: false,
+					defaultValue: "user",
+				},
 			},
-		},
-		timestamps: false,
+			{
+				sequelize,
+				tableName: "usuarios",
+				hooks: {
+					beforeCreate: async (user) => {
+						user.password = await bcrypt.hash(user.password, 8);
+					},
+				},
+				timestamps: false,
+			}
+		);
 	}
-);
+}
 
 module.exports = User;
