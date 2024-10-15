84d2d4bac5df7c308ef3f72bfcf34aa5cf6baad5
Thyago-ES <thyagorafael57@gmail.com>
2024-10-14 22:47:14 -0300

    feat: change functions to class AuthController for clean code

--- src/app/controllers/AuthController.js
+++ src/app/controllers/AuthController.js
@@ -2,49 +2,55 @@ const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-exports.register = async (req, res) => {
-	try {
-		let user = await User.findOne({ where: { email: req.body.email } });
-		if (user) {
-			return res.status(400).json({ error: "Email já registrado" });
-		}
-
-		user = await User.create(req.body);
-		res.status(201).json({
-			message: "Usuário criado com sucesso",
-			user: {
-				id: user.id,
-				name: user.name,
-			},
-		});
-	} catch (err) {
-		return res.status(500).json({ error: "Erro ao registrar usuário" });
-	}
-};
-
-exports.login = async (req, res) => {
-	try {
-		const { email, password } = req.body;
-
-		let user = await User.findOne({ where: { email } });
-		if (!user) {
-			return res.status(404).json({ error: "Usuário não encontrado" });
-		}
-
-		if (!(await bcrypt.compare(password, user.password))) {
-			return res.status(400).json({ error: "Credenciais incorretas" });
-		}
-
-		const token = jwt.sign(
-			{ id: user.id, role: user.role },
-			process.env.JWT_SECRET,
-			{
-				expiresIn: "1h",
+class AuthController {
+	async register(req, res) {
+		try {
+			let user = await User.findOne({ where: { email: req.body.email } });
+			if (user) {
+				return res.status(400).json({ error: "Email já registrado" });
 			}
-		);
 
-		return res.status(200).json({ token });
-	} catch (err) {
-		return res.status(500).json({ error: "Erro ao fazer login" });
+			user = await User.create(req.body);
+			res.status(201).json({
+				message: "Usuário criado com sucesso",
+				user: {
+					id: user.id,
+					name: user.name,
+				},
+			});
+		} catch (err) {
+			return res.status(500).json({ error: "Erro ao registrar usuário" });
+		}
 	}
-};
+
+	async login(req, res) {
+		try {
+			const { email, password } = req.body;
+
+			console.log("Erro não é aqui 1");
+
+			let user = await User.findOne({ where: { email } });
+			if (!user) {
+				return res.status(404).json({ error: "Usuário não encontrado" });
+			}
+
+			if (!(await bcrypt.compare(password, user.password))) {
+				return res.status(400).json({ error: "Credenciais incorretas" });
+			}
+
+			const token = jwt.sign(
+				{ id: user.id, role: user.role },
+				process.env.JWT_SECRET,
+				{
+					expiresIn: "1h",
+				}
+			);
+
+			return res.status(200).json({ token });
+		} catch (err) {
+			return res.status(500).json({ error: "Erro ao fazer login: " + err });
+		}
+	}
+}
+
+module.exports = new AuthController();
