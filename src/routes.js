8df7b5c997a09babe97b38ce1e0d7ef877e3f32e
Thyago-ES <thyagorafael57@gmail.com>
2024-10-14 22:53:53 -0300

    feat: rename router -> routes and authController -> AuthController

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,16 @@
 const { Router } = require("express");
 
-const authController = require("./app/controllers/authController");
+const AuthController = require("./app/controllers/AuthController");
 
 const authMiddleware = require("./app/middlewares/authMiddleware");
 
-const router = new Router();
+const routes = new Router();
 
-router.post("/register", authController.register);
-router.post("/login", authController.login);
+routes.post("/register", AuthController.register);
+routes.post("/login", AuthController.login);
 
-router.get("/protected", authMiddleware, (req, res) => {
+routes.get("/protected", authMiddleware, (req, res) => {
 	return res.status(200).json({ message: "Você está autenticado!" });
 });
 
-module.exports = router;
+module.exports = routes;
